cda996e52c291d2d97550dab17fb0e73c3bcf518
smvinoth2011 <smvinoth89@gmail.com>
2019-12-20 23:32:56 +0530

    added webpack production file and mode

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/client.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: 'india.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template: './templates/client.html'
+    }),
+  ],
+};
